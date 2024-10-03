6e875f624c6d97d0cea62786070bc4407fa092d3
Harshitha2308 <saiharshichenna@gmail.com>
2024-10-03 23:13:33 +0530

    first comment

---
+++ app/components/TodoList.tsx
@@ -0,0 +1,33 @@
+import { ITask } from "@/tasks"
+import React from "react";
+import Task from "./Task";
+interface TodoListProps{
+    tasks:ITask[];
+}
+
+const TodoList:React.FC<TodoListProps> = ({tasks}) => {
+  return (
+    <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+       
+        <th>Task</th>
+        
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+        {tasks.map(task=>(<Task key={task.id} task={task}/>
+      
+        ))}
+      
+      
+    </tbody>
+  </table>
+</div>
+  )
+}
+
+export default TodoList
